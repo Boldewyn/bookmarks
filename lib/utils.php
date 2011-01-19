00134e1aaf8f636b21bac4da345c31030d9dfd91
Manuel Strehl <git@manuel-strehl.de>
2011-01-19 07:14:31 +0100

    move template code to lib/utils

--- lib/utils.php
+++ lib/utils.php
@@ -18,3 +18,21 @@ function __($s) {
     return $s;
 }
 
+function _e($s) {
+    echo __($s);
+}
+
+function tpl($file, $ctx=array(), $safe=array()) {
+    foreach ($ctx as $k => $v) {
+        if (is_string($v) && ! in_array($k, $safe)) {
+            $ctx[$k] = h($v);
+        }
+    }
+    extract($ctx);
+    ob_start();
+    include("templates/$file.php");
+    $result = ob_get_contents();
+    ob_end_clean();
+    return $result;
+}
+
