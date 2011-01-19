256e0b335b86215afcb63c5d271e8ecd06ca4f63
Manuel Strehl <git@manuel-strehl.de>
2011-01-19 07:56:39 +0100

    add sample config file

---
+++ config.sample.php
@@ -0,0 +1,11 @@
+<?php
+
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', '3306');
+define('DB_NAME', 'bookmarks');
+define('DB_USER', 'bookmarks');
+define('DB_PWD', 'b00km4rk5');
+
+#define('OpenID', 'ASSUME_LOGGED_IN'); // For debugging only!
+define('OpenID', 'http://example.com/myopenid');
+
