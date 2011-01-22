b20c20a41ed17fee603abba10342815bea2da25e
Manuel Strehl <git@manuel-strehl.de>
2011-01-22 20:08:50 +0100

    add color support to icon creator

--- tools/create_icons.sh
+++ tools/create_icons.sh
@@ -1,8 +1,11 @@
 WIDTH=24
 MOD=""
+COLOR=black
 if [[ $1 == half ]]; then
   WIDTH=12
   MOD=.small
+else
+  COLOR=$1
 fi
 
 SOURCE=../static/icons.svg
@@ -11,13 +14,13 @@ PARAMS="--export-width=$WIDTH --export-height=$WIDTH"
 
 i=0
 for item in new list cancel logout; do
-  inkscape --export-png=${TARGET}$item$MOD.png --export-area=${i}00:104:${i}96:200 $PARAMS "$SOURCE" && identify ${TARGET}$item$MOD.png
+  sed "s/black/$COLOR/g" "$SOURCE" | inkscape --export-png=${TARGET}$item$MOD.png -a ${i}00:104:${i}96:200 $PARAMS /proc/self/fd/0
   let "i = $i + 1"
 done
 
 i=0
 for item in help save check login; do
-  inkscape --export-png=${TARGET}$item$MOD.png --export-area=${i}00:004:${i}96:100 $PARAMS "$SOURCE" && identify ${TARGET}$item$MOD.png
+  sed "s/black/$COLOR/g" "$SOURCE" | inkscape --export-png=${TARGET}$item$MOD.png -a ${i}00:004:${i}96:100 $PARAMS /proc/self/fd/0
   let "i = $i + 1"
 done
 
