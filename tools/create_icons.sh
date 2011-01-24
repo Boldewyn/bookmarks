888e7cf77dcf9b5dcfbbac12ec84614a5249c1b0
Manuel Strehl <git@manuel-strehl.de>
2011-01-24 07:44:46 +0100

    automatize icon creation

--- tools/create_icons.sh
+++ tools/create_icons.sh
@@ -20,39 +20,16 @@ PARAMS="--export-width=$WIDTH --export-height=$WIDTH"
 
 SOURCE=$(sed -e 's/opacity="\.3"/opacity="0"/' -e "s/black/$COLOR/" "$SOURCE_FILE")
 
-i=0
-for item in new list cancel logout; do
-  echo $SOURCE | inkscape --export-png=${TARGET}$item$MOD.png -a ${i}00:704:${i}96:800 $PARAMS /proc/self/fd/0
-  let "i = $i + 1"
-done
+ITEMS=$(sed -n '/id=/p' $SOURCE_FILE |grep translate | sed 's/.*id="\([^"]\+\)".\+transform="translate(\([0-9]\+\),\([0-9]\+\))".*$/\1:\2:\3/g')
 
-i=0
-for item in help save check login; do
-  echo $SOURCE | inkscape --export-png=${TARGET}$item$MOD.png -a ${i}00:604:${i}96:700 $PARAMS /proc/self/fd/0
-  let "i = $i + 1"
+for item in $ITEMS; do
+  X1=${item#*:}
+  X1=${X1%%:*}
+  let "X2 = $X1 + 24"
+  Y0=${item##*:}
+  let "Y2 = 200 - Y0"
+  let "Y1 = 200 - Y0 - 24"
+  item=${item%%:*}
+  echo $SOURCE | inkscape --export-png=${TARGET}$item$MOD.png \
+                 -a $X1:$Y1:$X2:$Y2 $PARAMS /proc/self/fd/0
 done
-
-i=0
-for item in excl 'export' pencil on-off; do
-  echo $SOURCE | inkscape --export-png=${TARGET}$item$MOD.png -a ${i}00:504:${i}96:600 $PARAMS /proc/self/fd/0
-  let "i = $i + 1"
-done
-
-i=0
-for item in tag plus minus refresh; do
-  echo $SOURCE | inkscape --export-png=${TARGET}$item$MOD.png -a ${i}00:404:${i}96:500 $PARAMS /proc/self/fd/0
-  let "i = $i + 1"
-done
-
-i=0
-for item in document magnifier folder cog; do
-  echo $SOURCE | inkscape --export-png=${TARGET}$item$MOD.png -a ${i}00:304:${i}96:400 $PARAMS /proc/self/fd/0
-  let "i = $i + 1"
-done
-
-i=0
-for item in cut; do
-  echo $SOURCE | inkscape --export-png=${TARGET}$item$MOD.png -a ${i}00:204:${i}96:300 $PARAMS /proc/self/fd/0
-  let "i = $i + 1"
-done
-
