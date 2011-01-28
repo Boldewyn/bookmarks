ae343600f4a22dda7e4b5b7b5c46e7c61681a4aa
Manuel Strehl <git@manuel-strehl.de>
2011-01-28 16:32:09 +0100

    add import, sync and installation routines

--- bookmark/index.php
+++ bookmark/index.php
@@ -68,6 +68,90 @@ if ($f === '') {
                 'bookmarks' => $bookmarks));
         }
     }
+} elseif ($f === 'import') {
+    $status = login();
+    if ($status !== True) {
+        messages_add(__('You need to login for this.'), 'error');
+        redirect('/login?from=import');
+    }
+    if (defined('DELICIOUS_AUTH')) {
+        $fp = fsockopen("ssl://api.delicious.com", 443);
+        if (!$fp) {
+            messages_add(__('Couldn’t connect to the delicious API server.'), 'error');
+            header('HTTP/1.0 500 Internal Server Error');
+            echo fetch($store);
+        } else {
+            $out = "GET /v1/posts/all HTTP/1.1\r\n";
+            $out .= "Host: api.delicious.com\r\n";
+            $out .= "User-Agent: Personal Bookmarks Manager\r\n";
+            $out .= "Authorization: Basic ".DELICIOUS_AUTH."\r\n";
+            $out .= "Connection: Close\r\n\r\n";
+            fwrite($fp, $out);
+            $data = "";
+            while (!feof($fp)) {
+                $data .= fgets($fp, 128);
+            }
+            fclose($fp);
+            $data = explode("\n\n", str_replace("\r\n", "\n", $data), 2);
+            $data = $data[1];
+            $xml = simplexml_load_string($data);
+            $posts = $xml->xpath('/posts/post');
+            $added = 0;
+            $needs_update = 0;
+            for ($i = 0; $i < count($posts); $i++) {
+                $atts = (array)$posts[$i]->attributes();
+                $atts += array("extended"=>"","shared"=>"yes");
+                $r = $store->save(
+                    $atts['url'],
+                    $atts['description'],
+                    explode(" ", $atts['tag']),
+                    $atts['extended'],
+                    ($atts['shared'] === 'no')
+                );
+                if ($r === True) {
+                    $added++;
+                } elseif ($r === Null) {
+                    $needs_update++;
+                }
+            }
+            messages_add(sprintf(__('Added %s new bookmarks.'), $added), 'success');
+            if ($needs_update > 0) {
+                messages_add(sprintf(__('%s bookmarks were already imported.'), $needs_update), 'info');
+            }
+            if (count($posts) > $added + $needs_update) {
+                messages_add(sprintf(__('%s bookmarks couldn’t be imported.'), count($posts) - $added -$needs_update), 'error');
+            }
+            redirect('/');
+        }
+    } else {
+        messages_add(__('You need to provide your access data for the delicious API server.'), 'error');
+        header('HTTP/1.0 500 Internal Server Error');
+        echo fetch($store);
+    }
+} elseif ($f === 'install') {
+    $status = login();
+    if ($status !== True) {
+        messages_add(__('You need to login for this.'), 'error');
+        redirect('/login?from=install');
+    }
+    $test = $db->query('SHOW TABLES LIKE "bookmark%"');
+    $not = array('bookmarks', 'bookmark_tags');
+    if ($test !== False) {
+        while ($row = $test->fetch(PDO::FETCH_NUM)) {
+            if (in_array($row[0], $not)) {
+                unset($not[array_search($row[0], $not)]);
+            }
+        }
+        if (count($not) === 0) {
+            $store->install();
+            messages_add(__('Installation successfully completed.'), 'success');
+        } else {
+            messages_add(__('There is already an installation.'), 'error');
+        }
+    } else {
+        messages_add(__('There was an error establishing the database connection.'), 'error');
+    }
+    echo fetch($store);
 } else {
     messages_add(sprintf(__('The site %s couldn’t be found.'), '<var>'.h(urlencode($f)).'</var>'),
         'error', True);
