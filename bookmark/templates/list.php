f01ccf19cd2170052035b937d864fcee9d4840d3
Manuel Strehl <git@manuel-strehl.de>
2011-09-09 20:02:29 +0200

    fine-tune templates

--- bookmark/templates/list.php
+++ bookmark/templates/list.php
@@ -2,16 +2,18 @@
   <ul id="bookmarks">
     <?php foreach ($bookmarks as $bookmark):?>
       <li>
-      <a class="url" rel="external" href="<?php echo $bookmark['url']?>"><?php echo $bookmark['title']?></a>
-      <span class="tags">
-        <?php foreach ($bookmark['tags'] as $tag):?>
-          <a rel="tag" href="<?php echo "${script_path}tags/".rawurlencode($tag)?>"><?php echo $tag?></a>
-        <?php endforeach?>
-      </span>
-      <span class="functions">
-        <a class="edit" href="<?php echo $script_path?>save?edit=1&amp;url=<?php echo rawurlencode($bookmark['url'])?>"><?php _e('edit')?></a>
-        <a class="edit" href="<?php echo $script_path?>delete?url=<?php echo rawurlencode($bookmark['url'])?>"><?php _e('delete')?></a>
-      </span>
+        <a class="url" rel="external" href="<?php echo $bookmark['url']?>"><?php echo $bookmark['title']?></a>
+        <span class="tags">
+          <?php foreach ($bookmark['tags'] as $tag):?>
+            <a rel="tag" href="<?php echo "${script_path}tags/".rawurlencode($tag)?>"><?php echo $tag?></a>
+          <?php endforeach?>
+        </span>
+        <?php if (logged_in()):?>
+          <span class="functions">
+            <a class="edit" href="<?php echo $script_path?>save?edit=1&amp;url=<?php echo rawurlencode($bookmark['url'])?>"><?php _e('edit')?></a>
+            <a class="edit" href="<?php echo $script_path?>delete?url=<?php echo rawurlencode($bookmark['url'])?>"><?php _e('delete')?></a>
+          </span>
+        <?php endif?>
       </li>
     <?php endforeach?>
   </ul>
--- bookmark/templates/pagination.php
+++ bookmark/templates/pagination.php
@@ -1,8 +1,10 @@
 <p><?php echo $all?> bookmarks</p>
-<ol class="paginate" start="0">
-  <li class="first<?php if ($i === 1):?> active<?php endif?>"><a href="<?php echo h(update_url(array('page'=>1)))?>"><?php _e('First')?></a></li>
-  <?php for ($i = 1; $i <= $pages; $i++):?>
-    <li<?php if ($i === $page):?> class="active"<?php endif?>><a href="<?php echo h(update_url(array('page'=>$i)))?>"><?php echo $i?></a></li>
-  <?php endfor?>
-  <li class="last<?php if ($i === $pages):?> active<?php endif?>"><a href="<?php echo h(update_url(array('page'=>$pages)))?>"><?php _e('Last')?></a></li>
-</ol>
+<?php if ($pages > 1):?>
+  <ol class="paginate" start="0">
+    <li class="first<?php if ($i === 1):?> active<?php endif?>"><a href="<?php echo h(update_url(array('page'=>1)))?>"><?php _e('First')?></a></li>
+    <?php for ($i = 1; $i <= $pages; $i++):?>
+      <li<?php if ($i === $page):?> class="active"<?php endif?>><a href="<?php echo h(update_url(array('page'=>$i)))?>"><?php echo $i?></a></li>
+    <?php endfor?>
+    <li class="last<?php if ($i === $pages):?> active<?php endif?>"><a href="<?php echo h(update_url(array('page'=>$pages)))?>"><?php _e('Last')?></a></li>
+  </ol>
+<?php endif?>
