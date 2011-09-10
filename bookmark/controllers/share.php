23edc948ec56046b7dfcdda10b2b7b4586faed6f
Manuel Strehl <git@manuel-strehl.de>
2011-09-10 23:49:16 +0200

    add CSRF token and handling for private bms to sharing

--- bookmark/controllers/share.php
+++ bookmark/controllers/share.php
@@ -18,10 +18,23 @@ function share($store) {
     }
     $service = v('service', '', 'post');
     if ($service === '') {
+        $add_priv = False;
+        if ($bookmark['private'] && v('share_private', '', 'post') !== '1') {
+            messages_add(__('This is a private bookmark. Do you really want to share it?'), 'info');
+            $add_priv = True;
+        }
         $r = tpl('share', array('body_id' => 'share',
             'site_title' => __('Share Bookmark'),
+            'add_priv' => $add_priv,
             'url' => $url,
             'bookmark' => $bookmark));
+    } else {
+        if (! check_csrf('share', v('ctoken', '', 'post'))) {
+            messages_add(__('You cannot share this bookmark without '.
+                'confirmation.'), 'error');
+            refer();
+        }
+        call_hook('share', array($service, $bookmark));
     }
     return $r;
 }
--- bookmark/templates/share.php
+++ bookmark/templates/share.php
@@ -9,6 +9,9 @@
   <p>
     <input type="hidden" name="url" value="<?php echo $url?>" />
     <input type="hidden" name="ctoken" value="<?php echo set_csrf('share')?>" />
+    <?php if(isset($add_priv) && $add_priv):?>
+      <input type="hidden" name="share_private" value="1" />
+    <?php endif?>
     <button type="submit" class="delete"><?php _e('Share')?></button>
     <button type="button" class="cancel" onclick="window.location.href='<?php echo $script_path?>'"><?php _e('Cancel')?></button>
   </p>
