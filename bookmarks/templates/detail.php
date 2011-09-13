8133fd6be533336b19e0c6a67dde3ccf4512dbaf
Manuel Strehl <git@manuel-strehl.de>
2011-09-13 15:33:56 +0200

    show shortcut on detail page

--- bookmarks/templates/detail.php
+++ bookmarks/templates/detail.php
@@ -1,10 +1,15 @@
 <?php if(isset($bookmark)):?>
   <div class="bookmark-detail">
-    <p><strong>URL:</strong> <span><?php echo $bookmark['url']?></span></p>
-    <p><strong>Title:</strong> <span><?php echo $bookmark['title']?></span></p>
-    <p><strong>Description:</strong> <span><?php echo $bookmark['notes']?></span></p>
-    <p><strong>Tags:</strong> <span><?php foreach ($bookmark['tags'] as $tag):?>
-      <a rel="tag" href="<?php echo "${script_path}tags/".rawurlencode($tag)?>"><?php echo $tag?></a>
-    <?php endforeach ?></span></p>
+    <p><strong><?php _e('Title:')?></strong> <span><?php echo $bookmark['title']?></span></p>
+    <p><strong><?php _e('URL:')?></strong> <a href="<?php echo $bookmark['url']?>"><?php echo $bookmark['url']?></a></p>
+    <?php if ($bookmark['notes']):?>
+      <p><strong><?php _e('Description:')?></strong> <span><?php echo $bookmark['notes']?></span></p>
+    <?php endif?>
+    <?php if (count($bookmark['tags'])):?>
+      <p><strong><?php _e('Tags:')?></strong> <span><?php foreach ($bookmark['tags'] as $tag):?>
+        <a rel="tag" href="<?php echo "${script_path}tags/".rawurlencode($tag)?>"><?php echo $tag?></a>
+      <?php endforeach ?></span></p>
+    <?php endif?>
+    <p><strong><?php _e('Short Link:')?></strong> <a href="<?php echo $script_path.'-'.$bookmark['shortcut']?>">http://<?php echo get_host().$script_path.'-'.$bookmark['shortcut']?></a></p>
   </div>
 <?php endif?>
