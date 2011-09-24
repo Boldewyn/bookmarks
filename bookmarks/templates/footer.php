9fd7b2b90ae2ec3e241187a6e6c4ab7a19127ee0
Manuel Strehl <git@manuel-strehl.de>
2011-09-24 21:45:07 +0200

    add feed link to footer

--- bookmarks/templates/footer.php
+++ bookmarks/templates/footer.php
@@ -1,4 +1,7 @@
       </article>
+      <footer>
+      <p><a href="<?php echo $script_path?>?type=rdf"><?php _e('Feed')?></a></p>
+      </footer>
     </div>
     <script src="<?php echo $base_path?>static/jquery.js"></script>
     <script src="<?php echo $base_path?>static/jquery-ui.js"></script>
