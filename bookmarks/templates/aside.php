67044f8a60894b90c817885ac2318a2658f52169
Manuel Strehl <git@manuel-strehl.de>
2011-09-24 21:42:24 +0200

    move tag cloud in a sidebar

--- bookmarks/templates/aside.php
+++ bookmarks/templates/aside.php
@@ -1,1 +1,5 @@
-<?php include 'tagcloud.php'?>
+<aside>
+  <h2><?php _e('Top Tags')?> <small><a href="<?php echo $script_path?>tags"><?php _e('(show all tags)')?></a></small></h2>
+  <?php include 'tagcloud.php'?>
+  <?php call_hook('front_aside')?>
+</aside>
